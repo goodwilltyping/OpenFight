bfe89e6a8fbf0612debc4bf91aa83d1804664054
andrew.nash <akahadaka@gmail.com>
2015-09-27 02:16:53 +0900

    Added a rudimentary JSON database

--- js/openfight.js
+++ js/openfight.js
@@ -1,13 +1,36 @@
 var OFAPP = OFAPP || {};
 
 OFAPP.file = {
-  readfile: function() {
-    console.log("hello world");
-  },
-  readpower: function() {
-    console.log("hello superpower");
-  },
-  readweapon: function() {
-    console.log("bicycle made of swords");
+  readfile: function(filename) {
+    console.log("getting data file: "+filename);
+    return OFDATA[filename];
+  }
+};
+
+OFAPP.fighter = {
+  get: function(tag) {
+    data = OFAPP.file.readfile("FIGHTERS");
+    index = Math.floor(Math.random() * data.length);
+    name0 = data[index];
+    console.log("getting fighter: "+name0);
+    tag.innerText = name0;
+  }
+};
+
+OFAPP.power = {
+  get: function() {
+    console.log("getting power: ");
+  }
+};
+
+OFAPP.weapon = {
+  get: function() {
+    console.log("getting weapon: ");
+  }
+};
+
+OFAPP.location = {
+  get: function() {
+    console.log("getting location: ");
   }
 };
