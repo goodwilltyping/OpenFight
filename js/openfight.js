0683f168696fcd0a5cd778b245efc2ce8b51dd78
andrew.nash <akahadaka@gmail.com>
2015-09-28 03:09:28 +0900

    Major update with code refactoring
    Changelog:
    - Updated to use a remote JSON file for data.
    - Remote file is editable in Google Spreadsheets
    - Deleted old son files
    - Changed layout and added rows
    - Refactored object names OFAPP to OF_APP and OFDATA to OF_DATA
    - Added dynamic HTML tag class when a link has been clicked
    - Added a basic refresh to the header when clicked
    - Changed “card” colours
    - Added a subheader
    - Added a footer
    - Added a box shadow
    - Adjusted widths to percentages so that narrow screens are supported.
    - Modified javascript to loop until a unique property is selected
    - Other small changes and fixes

--- js/openfight.js
+++ js/openfight.js
@@ -1,58 +1,94 @@
-var OFAPP = OFAPP || {};
+var OF_APP  = OF_APP  || {};
+var OF_DATA = OF_DATA || {};
 
-OFAPP.file = {
-  readfile: function(filename) {
-    console.log("getting data file: "+filename);
-    return OFDATA[filename];
+OF_APP.file = {
+  jsonp: function(url, callback) {
+    console.log("getting JSONP file data: "+url);
+    callbackName = 'jsonp_callback_' + Date.now();
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+  },
+
+  readfile: function() {
+    // File is a Google Docs spreadsheet
+    remote = "https://spreadsheets.google.com/feeds/list/15VybWTsJQ1N9NL-GTITUcDXtjC7x_b-c_LIWuS1-tPE/od6/public/values?alt=json-in-script";
+    OF_APP.file.jsonp(remote, function(data){
+      console.debug(data);
+      OF_DATA = data;
+    });
+  },
+
+  readdata: function(datatype) {
+    i = 0;
+    data = [];
+
+    while ((OF_DATA.feed.entry[i] !== undefined)
+    && (OF_DATA.feed.entry[i]["gsx$"+datatype.toLowerCase()]["$t"] !== "")) {
+      data[i] = OF_DATA.feed.entry[i++]["gsx$"+datatype.toLowerCase()]["$t"];
+    };
+    return data;
   }
 };
 
-OFAPP.fighters = {
+OF_APP.fighters = {
   get: function(tag) {
-    data = OFAPP.file.readfile("FIGHTERS");
-    index = Math.floor(Math.random() * data.length);
-    name0 = data[index];
-    console.log("getting fighter: "+name0);
-    tag.innerText = name0;
+    data = OF_APP.file.readdata("FIGHTERS");
+    do {
+      index = Math.floor(Math.random() * data.length);
+      name0 = data[index];
+      name1 = document.getElementById("fighter1").innerText;
+      name2 = document.getElementById("fighter2").innerText;
+      console.log("getting fighter: "+name0);
+      tag.innerText = name0;
+      tag.classList.add("active");
+    }  while ((name0 == name1) || (name0 == name2));
   }
 };
 
-OFAPP.powers = {
+OF_APP.powers = {
   get: function(tag) {
-    data = OFAPP.file.readfile("POWERS");
-    index = Math.floor(Math.random() * data.length);
-    name0 = data[index];
-    console.log("getting power: "+name0);
-    tag.innerText = name0
+    data = OF_APP.file.readdata("POWERS");
+    do {
+      index = Math.floor(Math.random() * data.length);
+      name0 = data[index];
+      name1 = document.getElementById("power1").innerText;
+      name2 = document.getElementById("power2").innerText;
+      console.log("getting power: "+name0);
+      tag.innerText = name0;
+      tag.classList.add("active");
+    }  while ((name0 == name1) || (name0 == name2));
   }
 };
 
-OFAPP.weapons = {
+OF_APP.weapons = {
   get: function(tag) {
-    data = OFAPP.file.readfile("WEAPONS");
-    index = Math.floor(Math.random() * data.length);
-    name0 = data[index];
-    console.log("getting weapon: "+name0);
-    tag.innerText = name0
+    data = OF_APP.file.readdata("WEAPONS");
+    do {
+      index = Math.floor(Math.random() * data.length);
+      name0 = data[index];
+      name1 = document.getElementById("weapon1").innerText;
+      name2 = document.getElementById("weapon2").innerText;
+      console.log("getting weapon: "+name0);
+      tag.innerText = name0;
+      tag.classList.add("active");
+    }  while ((name0 == name1) || (name0 == name2));
   }
 };
 
-OFAPP.location = {
+OF_APP.location = {
   get: function(tag) {
-    data = OFAPP.file.readfile("LOCATIONS");
+    data = OF_APP.file.readdata("LOCATIONS");
     index = Math.floor(Math.random() * data.length);
     name0 = data[index];
     console.log("getting location: "+name0);
-    tag.innerText = name0
-  }
-};
-
-OFAPP.nothing = {
-  get: function(tag) {
-    data = OFAPP.file.readfile("NOTHING");
-    index = Math.floor(Math.random() * data.length);
-    name0 = data[index];
-    console.log("getting nothing: "+name0);
-    tag.innerText = name0
+    tag.innerText = name0;
+    tag.classList.add("active")
   }
 };
