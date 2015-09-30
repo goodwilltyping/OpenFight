0c31971a4ac0297853f72c8cb106a7d11a352d15
andrew.nash <akahadaka@gmail.com>
2015-09-30 13:26:26 +0900

    Added Online/Offline Check
    Check determines whether to download new database or operate from local
    storage only

--- js/openfight.js
+++ js/openfight.js
@@ -17,16 +17,26 @@ OF_APP.file = {
   },
 
   readfile: function() {
-    // File is a Google Docs spreadsheet
-    remote = "https://spreadsheets.google.com/feeds/list/15VybWTsJQ1N9NL-GTITUcDXtjC7x_b-c_LIWuS1-tPE/od6/public/values?alt=json-in-script";
-    OF_APP.file.jsonp(remote, function(data){
-      console.debug(data);
-      OF_DATA = data;
-      OF_APP.file.writelocal("FIGHTERS", OF_APP.file.readdata("FIGHTERS"));
-      OF_APP.file.writelocal("POWERS", OF_APP.file.readdata("POWERS"));
-      OF_APP.file.writelocal("WEAPONS", OF_APP.file.readdata("WEAPONS"));
-      OF_APP.file.writelocal("LOCATIONS", OF_APP.file.readdata("LOCATIONS"));
-    });
+    if (navigator.onLine) {
+      // Get data from a shared Google Docs spreadsheet published to web and read as JSONP
+      remote = "https://spreadsheets.google.com/feeds/list/15VybWTsJQ1N9NL-GTITUcDXtjC7x_b-c_LIWuS1-tPE/od6/public/values?alt=json-in-script";
+      OF_APP.file.jsonp(remote, function(data){
+        console.debug(data);
+        OF_DATA = data;
+        OF_APP.file.writelocal("FIGHTERS", OF_APP.file.readdata("FIGHTERS"));
+        OF_APP.file.writelocal("POWERS", OF_APP.file.readdata("POWERS"));
+        OF_APP.file.writelocal("WEAPONS", OF_APP.file.readdata("WEAPONS"));
+        OF_APP.file.writelocal("LOCATIONS", OF_APP.file.readdata("LOCATIONS"));
+      });
+    } else {
+      // CHeck localstorage for existing data
+      if (data1 = OF_APP.file.readlocal("FIGHTERS")) {
+        console.log("getting JSONP from localstorage");
+        console.debug(data1);
+      } else {
+        alert ("No internet connection detected and no local database available.\nConnect at least once to download the database");
+      }
+    }
   },
 
   readdata: function(datatype) {
