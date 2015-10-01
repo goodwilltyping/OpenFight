728de52ff22afaf03eaa2e35927972c2215f464e
andrew.nash <akahadaka@gmail.com>
2015-10-01 18:30:34 +0900

    Cards picked only once
    Cards randomly drawn are removed from the set and cannot be drawn again
    until either the end of the set, or the deck is reshuffled by the user
    (no reshuffle button exists yet, but refreshing the page performs the
    same function)

--- js/openfight.js
+++ js/openfight.js
@@ -5,6 +5,10 @@ var OF_DATA = OF_DATA || {};
  * GAME
  */
 OF_APP.game = {
+  load: function() {
+    OF_APP.file.readFile();
+  },
+
   new: function() {
     fighter1  = document.getElementById("fighter1");
     fighter2  = document.getElementById("fighter2");
@@ -43,46 +47,64 @@ OF_APP.file = {
     document.body.appendChild(script);
   },
 
-  readfile: function() {
+  readFile: function() {
+    data = [];
+    // Check if the app is online
     if (navigator.onLine) {
       // Get data from a shared Google Docs spreadsheet published to web and read as JSONP
       remote = "https://spreadsheets.google.com/feeds/list/15VybWTsJQ1N9NL-GTITUcDXtjC7x_b-c_LIWuS1-tPE/od6/public/values?alt=json-in-script";
       OF_APP.file.jsonp(remote, function(data){
         console.debug(data);
-        OF_DATA = data;
-        OF_APP.file.writelocal("FIGHTERS", OF_APP.file.readdata("FIGHTERS"));
-        OF_APP.file.writelocal("POWERS", OF_APP.file.readdata("POWERS"));
-        OF_APP.file.writelocal("WEAPONS", OF_APP.file.readdata("WEAPONS"));
-        OF_APP.file.writelocal("LOCATIONS", OF_APP.file.readdata("LOCATIONS"));
+        OF_APP.file.writeLocal("openfight.json", data);
+        OF_APP.file.writeLocal("FIGHTERS", OF_APP.file.readData("FIGHTERS", data));
+        OF_APP.file.writeLocal("POWERS", OF_APP.file.readData("POWERS", data));
+        OF_APP.file.writeLocal("WEAPONS", OF_APP.file.readData("WEAPONS", data));
+        OF_APP.file.writeLocal("LOCATIONS", OF_APP.file.readData("LOCATIONS", data));
       });
     } else {
-      // CHeck localstorage for existing data
-      if (data1 = OF_APP.file.readlocal("FIGHTERS")) {
-        console.log("getting JSONP from localstorage");
-        console.debug(data1);
+      // Check local storage for existing data
+      if (data = OF_APP.file.readLocal("openfight.json")) {
+        console.log("getting JSONP from local storage");
+        console.debug(data);
+        // Reset the local database
+        OF_APP.file.writeLocal("FIGHTERS", OF_APP.file.readData("FIGHTERS", data));
+        OF_APP.file.writeLocal("POWERS", OF_APP.file.readData("POWERS", data));
+        OF_APP.file.writeLocal("WEAPONS", OF_APP.file.readData("WEAPONS", data));
+        OF_APP.file.writeLocal("LOCATIONS", OF_APP.file.readData("LOCATIONS", data));
       } else {
         alert ("No internet connection detected and no local database available.\nConnect at least once to download the database.");
       }
     }
   },
 
-  readdata: function(datatype) {
+  readData: function(dataType, dataSource) {
     i = 0;
     data = [];
 
-    while ((OF_DATA.feed.entry[i] !== undefined)
-    && (OF_DATA.feed.entry[i]["gsx$"+datatype.toLowerCase()]["$t"] !== "")) {
-      data[i] = OF_DATA.feed.entry[i++]["gsx$"+datatype.toLowerCase()]["$t"];
+    while ((dataSource.feed.entry[i] !== undefined)
+    && (dataSource.feed.entry[i]["gsx$"+dataType.toLowerCase()]["$t"] !== "")) {
+      data[i] = dataSource.feed.entry[i]["gsx$"+dataType.toLowerCase()]["$t"];
+      i++;
     };
     return data;
   },
 
-  writelocal: function(key, value){
+  removeData: function(dataType, index) {
+    i = 0;
+    dataSource = OF_APP.file.readLocal(dataType);
+
+    // Save the modified deck back to local storage
+    data = dataSource.splice(index, 1);
+    console.log("removing "+dataType+": "+data);
+    OF_APP.file.writeLocal(dataType, dataSource);
+  },
+
+  writeLocal: function(key, value){
     localStorage.setItem(key, JSON.stringify(value));
   },
 
-  readlocal: function(key){
-    var value = localStorage.getItem(key);
+  readLocal: function(key){
+    value = localStorage.getItem(key);
     return value && JSON.parse(value);
   }
 };
@@ -94,16 +116,30 @@ OF_APP.fighters = {
   get: function(tag) {
     // Check that button is not already active
     if (!tag.classList.contains("active")) {
-      data = OF_APP.file.readlocal("FIGHTERS");
+      data = OF_APP.file.readLocal("FIGHTERS");
+
+      // Check that there are still cards in this set
+      if (data.length == 0) {
+        // If not, then load the full set again
+        console.log("reshuffle set: FIGHTERS");
+        dataSource = OF_APP.file.readLocal("openfight.json");
+        data = OF_APP.file.readData("FIGHTERS", dataSource);
+        OF_APP.file.writeLocal("FIGHTERS", data);
+      }
+
       name1 = document.getElementById("fighter1").innerText;
       name2 = document.getElementById("fighter2").innerText;
+
       // Loop values until a unique one is found
       do {
         index = Math.floor(Math.random() * data.length);
         name0 = data[index];
       }  while ((name0 == name1) || (name0 == name2));
-      // Update the page
+
       console.log("getting fighter: "+name0);
+      OF_APP.file.removeData("FIGHTERS", index);
+
+      // Update the page
       tag.innerText = name0;
       tag.classList.add("active");
     }
@@ -111,7 +147,6 @@ OF_APP.fighters = {
 
   reset: function(tag) {
     console.log("resetting fighter...");
-    console.log(tag);
     tag.innerText = "Select your character";
     tag.classList.remove("active");
   }
@@ -121,16 +156,30 @@ OF_APP.powers = {
   get: function(tag) {
     // Check that button is not already active
     if (!tag.classList.contains("active")) {
-      data = OF_APP.file.readlocal("POWERS");
+      data = OF_APP.file.readLocal("POWERS");
+
+      // Check that there are still cards in this set
+      if (data.length == 0) {
+        // If not, then load the full set again
+        console.log("reshuffle set: POWERS");
+        dataSource = OF_APP.file.readLocal("openfight.json");
+        data = OF_APP.file.readData("POWERS", dataSource);
+        OF_APP.file.writeLocal("POWERS", data);
+      }
+
       name1 = document.getElementById("power1").innerText;
       name2 = document.getElementById("power2").innerText;
+
       // Loop values until a unique one is found
       do {
         index = Math.floor(Math.random() * data.length);
         name0 = data[index];
       }  while ((name0 == name1) || (name0 == name2));
-      // Update the page
+
       console.log("getting power: "+name0);
+      OF_APP.file.removeData("POWERS", index);
+
+      // Update the page
       tag.innerText = name0;
       tag.classList.add("active");
     }
@@ -147,16 +196,30 @@ OF_APP.weapons = {
   get: function(tag) {
     // Check that button is not already active
     if (!tag.classList.contains("active")) {
-      data = OF_APP.file.readlocal("WEAPONS");
+      data = OF_APP.file.readLocal("WEAPONS");
+
+      // Check that there are still cards in this set
+      if (data.length == 0) {
+        // If not, then load the full set again
+        console.log("reshuffle set: WEAPONS");
+        dataSource = OF_APP.file.readLocal("openfight.json");
+        data = OF_APP.file.readData("WEAPONS", dataSource);
+        OF_APP.file.writeLocal("WEAPONS", data);
+      }
+
       name1 = document.getElementById("weapon1").innerText;
       name2 = document.getElementById("weapon2").innerText;
+
       // Loop values until a unique one is found
       do {
         index = Math.floor(Math.random() * data.length);
         name0 = data[index];
       }  while ((name0 == name1) || (name0 == name2));
-      // Update the page
+
       console.log("getting weapon: "+name0);
+      OF_APP.file.removeData("WEAPONS", index);
+
+      // Update the page
       tag.innerText = name0;
       tag.classList.add("active");
     }
@@ -173,9 +236,28 @@ OF_APP.locations = {
   get: function(tag) {
     // Check that button is not already active
     if (!tag.classList.contains("active")) {
-      data = OF_APP.file.readlocal("LOCATIONS");
-      index = Math.floor(Math.random() * data.length);
-      name0 = data[index];
+      data = OF_APP.file.readLocal("LOCATIONS");
+
+      // Check that there are still cards in this set
+      if (data.length == 0) {
+        // If not, then load the full set again
+        console.log("reshuffle set: LOCATIONS");
+        dataSource = OF_APP.file.readLocal("openfight.json");
+        data = OF_APP.file.readData("LOCATIONS", dataSource);
+        OF_APP.file.writeLocal("LOCATIONS", data);
+      }
+
+      name1 = document.getElementById("location1").innerText;
+
+      // Loop values until a unique one is found
+      do {
+        index = Math.floor(Math.random() * data.length);
+        name0 = data[index];
+      }  while (name0 == name1);
+
+      console.log("getting location: "+name0);
+      OF_APP.file.removeData("LOCATIONS", index);
+
       // Update the page
       console.log("getting location: "+name0);
       tag.innerText = name0;
